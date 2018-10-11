8d2f5adc992cb5f3af0ebb328219dc74f7bb108b
princesso <egbunaoluebube@gmail.com>
2018-10-11 13:59:00 +0100

    add functions to handle changing colors and effecting them on background

---
+++ index.js
@@ -0,0 +1,10 @@
+var h3value = document.querySelector("h3");
+var firstColor =document.querySelector(".color1");
+var secondColor= document.querySelector(".color2");
+
+firstColor.addEventListener("input",function(){
+
+})
+secondColor.addEventListener("input",function(){
+  
+})
