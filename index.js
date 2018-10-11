d6e958e19a1debcd9c975f40e20cd5c969c9f12e
princesso <egbunaoluebube@gmail.com>
2018-10-11 14:29:20 +0100

    optimize function for changing gradient

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var h3value = document.querySelector("h3");
 var firstColor =document.querySelector(".color1");
 var secondColor= document.querySelector(".color2");
+var bodyBackground=document.getElementById("body");
 
-firstColor.addEventListener("input",function(){
-
-})
-secondColor.addEventListener("input",function(){
-  
-})
+function changeBackground() {
+  bodyBackground.style.background=
+  "linear-gradient(to right, "
+  +firstColor.value
+  +"," 
+  +secondColor.value
+  +")";
+}
+firstColor.addEventListener("input",changeBackground)
+secondColor.addEventListener("input",changeBackground)
